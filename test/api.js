c6592b700598a5e8b280c9fa59c48b9e5338ba63
william <wwwy3y3@gmail.com>
2015-01-24 22:39:36 +0800

    add some test

--- test/api.js
+++ test/api.js
@@ -2,6 +2,19 @@ var cnHbs= require('../');
 var source = "{{{name}}}<p>Hello, my name is {{name}}. {{cool/qwe}}. I am from {{hometown}}. I have " +
              "{{kids.length}} kids:</p>" +
              "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>, so hometown: {{hometown}}";
-var matches= cnHbs.findMustache(source, true);
-//console.log(matches);
-console.log(cnHbs.encloseHtmlTag(source, 'x-cn', { class: "cn-key pupu me", ur: 'test', key: '$' }));
+
+// find all mustache, include inblock mustaches
+var matches= cnHbs.findMustache(source);
+console.log(matches);
+
+// wrapWith
+var newstring= cnHbs.warpWith(source, "##", "##");
+console.log(newstring);
+
+// enclose with html tag
+console.log(
+	cnHbs.encloseHtmlTag(source, 
+		'x-cn', 
+		{ class: "cn-key pupu me", ur: 'test', key: '$' }
+		)
+	);
